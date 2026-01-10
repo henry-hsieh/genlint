9c1ef931440bf8e406d7564d9e1af04bb1be87cc
Henry Hsieh <r901042004@yahoo.com.tw>
2026-01-10 23:27:06 +0800

    docs: binary detection, control-char, max-errors, and max-warnings (#36)

--- completions/_genlint.ps1
+++ completions/_genlint.ps1
@@ -21,8 +21,48 @@ Register-ArgumentCompleter -Native -CommandName 'genlint' -ScriptBlock {
 
     $completions = @(switch ($command) {
         'genlint' {
+            [CompletionResult]::new('-i', '-i', [CompletionResultType]::ParameterName, 'Input file(s) to lint')
+            [CompletionResult]::new('--input', '--input', [CompletionResultType]::ParameterName, 'Input file(s) to lint')
+            [CompletionResult]::new('-e', '-e', [CompletionResultType]::ParameterName, 'Glob patterns to exclude')
+            [CompletionResult]::new('--exclude', '--exclude', [CompletionResultType]::ParameterName, 'Glob patterns to exclude')
+            [CompletionResult]::new('-f', '-f', [CompletionResultType]::ParameterName, 'Output format')
+            [CompletionResult]::new('--format', '--format', [CompletionResultType]::ParameterName, 'Output format')
+            [CompletionResult]::new('-o', '-o', [CompletionResultType]::ParameterName, 'Output file path')
+            [CompletionResult]::new('--output', '--output', [CompletionResultType]::ParameterName, 'Output file path')
+            [CompletionResult]::new('-d', '-d', [CompletionResultType]::ParameterName, 'Disable specific checks')
+            [CompletionResult]::new('--disable', '--disable', [CompletionResultType]::ParameterName, 'Disable specific checks')
+            [CompletionResult]::new('-l', '-l', [CompletionResultType]::ParameterName, 'Maximum allowed line length')
+            [CompletionResult]::new('--max-line-length', '--max-line-length', [CompletionResultType]::ParameterName, 'Maximum allowed line length')
+            [CompletionResult]::new('-c', '-c', [CompletionResultType]::ParameterName, 'Maximum allowed consecutive blank lines')
+            [CompletionResult]::new('--max-consecutive-blank', '--max-consecutive-blank', [CompletionResultType]::ParameterName, 'Maximum allowed consecutive blank lines')
+            [CompletionResult]::new('--max-errors', '--max-errors', [CompletionResultType]::ParameterName, 'Maximum number of errors to report (set to 0 to disable)')
+            [CompletionResult]::new('--max-warnings', '--max-warnings', [CompletionResultType]::ParameterName, 'Maximum number of warnings to report (set to 0 to disable)')
+            [CompletionResult]::new('-s', '-s', [CompletionResultType]::ParameterName, 'Read input from stdin')
+            [CompletionResult]::new('--stdin', '--stdin', [CompletionResultType]::ParameterName, 'Read input from stdin')
+            [CompletionResult]::new('-a', '-a', [CompletionResultType]::ParameterName, 'Treat all input as text, bypassing binary detection')
+            [CompletionResult]::new('--text', '--text', [CompletionResultType]::ParameterName, 'Treat all input as text, bypassing binary detection')
             [CompletionResult]::new('-h', '-h', [CompletionResultType]::ParameterName, 'Print help')
             [CompletionResult]::new('--help', '--help', [CompletionResultType]::ParameterName, 'Print help')
+            [CompletionResult]::new('-V', '-V ', [CompletionResultType]::ParameterName, 'Print version')
+            [CompletionResult]::new('--version', '--version', [CompletionResultType]::ParameterName, 'Print version')
+            [CompletionResult]::new('generate-completion', 'generate-completion', [CompletionResultType]::ParameterValue, 'Generate shell completions')
+            [CompletionResult]::new('help', 'help', [CompletionResultType]::ParameterValue, 'Print this message or the help of the given subcommand(s)')
+            break
+        }
+        'genlint;generate-completion' {
+            [CompletionResult]::new('-h', '-h', [CompletionResultType]::ParameterName, 'Print help')
+            [CompletionResult]::new('--help', '--help', [CompletionResultType]::ParameterName, 'Print help')
+            break
+        }
+        'genlint;help' {
+            [CompletionResult]::new('generate-completion', 'generate-completion', [CompletionResultType]::ParameterValue, 'Generate shell completions')
+            [CompletionResult]::new('help', 'help', [CompletionResultType]::ParameterValue, 'Print this message or the help of the given subcommand(s)')
+            break
+        }
+        'genlint;help;generate-completion' {
+            break
+        }
+        'genlint;help;help' {
             break
         }
     })
--- completions/genlint.fish
+++ completions/genlint.fish
@@ -1,1 +1,51 @@
-complete -c genlint -s h -l help -d 'Print help'
+# Print an optspec for argparse to handle cmd's options that are independent of any subcommand.
+function __fish_genlint_global_optspecs
+	string join \n s/stdin i/input= e/exclude= f/format= o/output= d/disable= a/text l/max-line-length= c/max-consecutive-blank= max-errors= max-warnings= h/help V/version
+end
+
+function __fish_genlint_needs_command
+	# Figure out if the current invocation already has a command.
+	set -l cmd (commandline -opc)
+	set -e cmd[1]
+	argparse -s (__fish_genlint_global_optspecs) -- $cmd 2>/dev/null
+	or return
+	if set -q argv[1]
+		# Also print the command, so this can be used to figure out what it is.
+		echo $argv[1]
+		return 1
+	end
+	return 0
+end
+
+function __fish_genlint_using_subcommand
+	set -l cmd (__fish_genlint_needs_command)
+	test -z "$cmd"
+	and return 1
+	contains -- $cmd[1] $argv
+end
+
+complete -c genlint -n "__fish_genlint_needs_command" -s i -l input -d 'Input file(s) to lint' -r
+complete -c genlint -n "__fish_genlint_needs_command" -s e -l exclude -d 'Glob patterns to exclude' -r
+complete -c genlint -n "__fish_genlint_needs_command" -s f -l format -d 'Output format' -r -f -a "json\t''
+jsonl\t''
+plain\t''"
+complete -c genlint -n "__fish_genlint_needs_command" -s o -l output -d 'Output file path' -r -F
+complete -c genlint -n "__fish_genlint_needs_command" -s d -l disable -d 'Disable specific checks' -r -f -a "mix-indent\t''
+trailing-space\t''
+conflict-marker\t''
+long-line\t''
+consecutive-blank\t''
+final-newline\t''"
+complete -c genlint -n "__fish_genlint_needs_command" -s l -l max-line-length -d 'Maximum allowed line length' -r
+complete -c genlint -n "__fish_genlint_needs_command" -s c -l max-consecutive-blank -d 'Maximum allowed consecutive blank lines' -r
+complete -c genlint -n "__fish_genlint_needs_command" -l max-errors -d 'Maximum number of errors to report (set to 0 to disable)' -r
+complete -c genlint -n "__fish_genlint_needs_command" -l max-warnings -d 'Maximum number of warnings to report (set to 0 to disable)' -r
+complete -c genlint -n "__fish_genlint_needs_command" -s s -l stdin -d 'Read input from stdin'
+complete -c genlint -n "__fish_genlint_needs_command" -s a -l text -d 'Treat all input as text, bypassing binary detection'
+complete -c genlint -n "__fish_genlint_needs_command" -s h -l help -d 'Print help'
+complete -c genlint -n "__fish_genlint_needs_command" -s V -l version -d 'Print version'
+complete -c genlint -n "__fish_genlint_needs_command" -f -a "generate-completion" -d 'Generate shell completions'
+complete -c genlint -n "__fish_genlint_needs_command" -f -a "help" -d 'Print this message or the help of the given subcommand(s)'
+complete -c genlint -n "__fish_genlint_using_subcommand generate-completion" -s h -l help -d 'Print help'
+complete -c genlint -n "__fish_genlint_using_subcommand help; and not __fish_seen_subcommand_from generate-completion help" -f -a "generate-completion" -d 'Generate shell completions'
+complete -c genlint -n "__fish_genlint_using_subcommand help; and not __fish_seen_subcommand_from generate-completion help" -f -a "help" -d 'Print this message or the help of the given subcommand(s)'
