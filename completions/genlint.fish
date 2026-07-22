607608980b53a614d09c3657730ba6d60127861e
Henry Hsieh <r901042004@yahoo.com.tw>
2026-07-22 20:17:38 +0800

    chore: update completion scripts

--- completions/genlint.fish
+++ completions/genlint.fish
@@ -1,27 +1,27 @@
 # Print an optspec for argparse to handle cmd's options that are independent of any subcommand.
 function __fish_genlint_global_optspecs
-	string join \n s/stdin i/input= e/exclude= f/format= o/output= d/disable= a/text l/max-line-length= c/max-consecutive-blank= max-errors= max-warnings= max-info= m/conflict-marker-style= h/help V/version
+    string join \n s/stdin i/input= e/exclude= f/format= o/output= d/disable= a/text l/max-line-length= c/max-consecutive-blank= max-errors= max-warnings= max-info= m/conflict-marker-style= h/help V/version
 end
 
 function __fish_genlint_needs_command
-	# Figure out if the current invocation already has a command.
-	set -l cmd (commandline -opc)
-	set -e cmd[1]
-	argparse -s (__fish_genlint_global_optspecs) -- $cmd 2>/dev/null
-	or return
-	if set -q argv[1]
-		# Also print the command, so this can be used to figure out what it is.
-		echo $argv[1]
-		return 1
-	end
-	return 0
+    # Figure out if the current invocation already has a command.
+    set -l cmd (commandline -opc)
+    set -e cmd[1]
+    argparse -s (__fish_genlint_global_optspecs) -- $cmd 2>/dev/null
+    or return
+    if set -q argv[1]
+        # Also print the command, so this can be used to figure out what it is.
+        echo $argv[1]
+        return 1
+    end
+    return 0
 end
 
 function __fish_genlint_using_subcommand
-	set -l cmd (__fish_genlint_needs_command)
-	test -z "$cmd"
-	and return 1
-	contains -- $cmd[1] $argv
+    set -l cmd (__fish_genlint_needs_command)
+    test -z "$cmd"
+    and return 1
+    contains -- $cmd[1] $argv
 end
 
 complete -c genlint -n "__fish_genlint_needs_command" -s i -l input -d 'Input file(s) to lint' -r
